922c90a6538277215f0054800a6e002681ef3c81
Javier Maria <javier.maria.salas@gmail.com>
2022-10-22 14:03:51 -0500

    Added api-key from the dog api

--- assets/index.js
+++ assets/index.js
@@ -1,5 +1,6 @@
-//const DOG_API = 'https://api.thecatapi.com/v1/images/search';
+//const CAT_API = 'https://api.thecatapi.com/v1/images/search';
 const DOG_API = 'https://api.thedogapi.com/v1/images/search';
+const DOG_API_KEY = 'live_PMi8BLxFPYAnCgsqmXleBgJkXw2R92PqxaWbPzdSIcETYlcEsmDYTfyDGzfc1epH';
 const LIMIT = 3;
 const button = document.getElementById('');
 
@@ -16,7 +17,7 @@ const asignDogPhoto = async () => {
             document.getElementById(`img-container${index+1}`)
                 .innerHTML = `
                     <img id="Dog-Image" class="Dog-Image" src="${element.url}" alt="Cat Random Picture">
-                `   
+                `; 
         }); 
     } catch (error) {
         console.error(error);
